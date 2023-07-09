30cc6b1a6cc8736f2ff49c753a374f6cf91184cf
unknown <etomarat@gmail.com>
2023-07-10 02:16:26 +0300

    save litle more info of items

--- media/lua/client/ZipContainer_client.lua
+++ media/lua/client/ZipContainer_client.lua
@@ -3,9 +3,17 @@ local MOD_NAME = 'ZipContainer'
 ---@class ItemTable
 ---@field id integer
 ---@field condition number
+---@field age number
 ---@field delta number
 ---@field hunger number
 ---@field weight number
+---@field isBroken boolean
+---@field isCooked boolean
+---@field cookedString string
+---@field isBurnt boolean
+---@field burntString string
+
+---@alias zipTable table<string, ItemTable[]>
 
 ---@class ZipContainer
 -- -@field itemContainer ItemContainer
@@ -24,9 +32,11 @@ function ZipContainer:new(container)
     -- ---@type IsoObject
     -- o.isoObject = container:getParent()
     -- o.modData = o.isoObject:getModData()[MOD_NAME] or {}
+    ---@type ItemContainer
     self.itemContainer = container
     ---@type IsoObject
     self.isoObject = container:getParent()
+    ---@type zipTable
     self.modData = o.isoObject:getModData()[MOD_NAME] or {}
     -- print('modData', bcUtils.dump(o.modData))
     return self
@@ -45,6 +55,17 @@ function ZipContainer:makeItems()
             local item = InventoryItemFactory.CreateItem(type);
             item:setID(typeTable.id)
             item:setCondition(typeTable.condition)
+            item:setAge(typeTable.age)
+            item:setBroken(typeTable.isBroken)
+
+            if typeTable.isCooked then
+                item:setCooked(typeTable.isCooked)
+                item:setCookedString(typeTable.cookedString)
+            end
+            if typeTable.isBurnt then
+                item:setBurnt(typeTable.isBurnt)
+                item:setBurntString(typeTable.burntString)
+            end
             if typeTable.delta then
                 item = item  --[[@as DrainableComboItem]]
                 item:setDelta(typeTable.delta)
@@ -59,28 +80,89 @@ function ZipContainer:makeItems()
     return self.itemContainer
 end
 
+local function clone(item)
+    local newItem = InventoryItemFactory.CreateItem(item:getFullType())
+    if not newItem then return end
+    -- newItem:setAge(item:getAge())
+    -- newItem:setCondition(item:getCondition(), false)
+    newItem:getVisual():copyFrom(item:getVisual()) -- !
+    -- newItem:setBroken(item:isBroken())
+    newItem:setColor(item:getColor())
+    -- if item:isCooked() then
+    --     newItem:setCooked(item:isCooked())
+    --     newItem:setCookedString(item:getCookedString())
+    -- end
+    -- if item:isBurnt() then
+    --     newItem:setBurnt(item:isBurnt())
+    --     newItem:setBurntString(item:getBurntString())
+    -- end
+    newItem:setDisplayName(item:getDisplayName())
+
+    if item:hasModData() then
+        newItem:copyModData(item:getModData())
+    end
+    if instanceof(item, "Clothing") then
+        item:copyPatchesTo(newItem)
+        newItem:setBloodLevel(item:getBloodLevel())
+        newItem:setDirtyness(item:getDirtyness())
+        newItem:setPalette(item:getPalette())
+        newItem:setWetness(item:getWetness())
+    end
+    if instanceof(item, "DrainableComboItem") then
+        newItem:setUsedDelta(item:getUsedDelta())
+        newItem:updateWeight()
+    end
+    if instanceof(item, "Food") then
+        newItem:setCalories(item:getCalories())
+        newItem:setCarbohydrates(item:getCarbohydrates())
+        newItem:setProteins(item:getProteins())
+        newItem:setLipids(item:getLipids())
+        newItem:setWeight(item:getWeight())
+        newItem:setHungChange(item:getHungChange())
+        newItem:setUnhappyChange(item:getUnhappyChange())
+        newItem:setBoredomChange(item:getBoredomChange())
+        newItem:setStressChange(item:getStressChange())
+        newItem:setEnduranceChange(item:getEnduranceChange())
+        newItem:setPainReduction(item:getPainReduction())
+        newItem:setThirstChange(item:getThirstChange())
+        newItem:setCookedInMicrowave(item:isCookedInMicrowave())
+        newItem:setSpices(item:getSpices())
+    end
+end
+
 ---@param items InventoryItem[]
 function ZipContainer:addItems(items)
     for key, item in pairs(items) do
         local type = item:getFullType()
-        local typeTable = self.modData[type] or {}
-        local delta = nil
-        local hunger = nil
-        if item:IsDrainable() then
-            local drainableItem = item --[[@as DrainableComboItem]]
-            delta = drainableItem:getDelta()
-        end
-        if item:IsFood() then -- TODO: обрабатывать все свойства
-            local foodItem = item --[[@as Food]]
-            hunger = foodItem:getHungChange()
-        end
-        table.insert(typeTable, {
+        local typeTable = self.modData[type] or {} --[[@as zipTable]]
+        local resultTable = {
             id = item:getID(),
             condition = item:getCondition(),
             weight = item:getUnequippedWeight(),
-            delta = delta,
-            hunger = hunger
-        })
+            age = item:getAge(),
+            isBroken = item:isBroken(),
+        }
+        print('item:getVisual(): ', item:getVisual()) -- относится к одежде. Там много параметров. Вероятно проще запретить хранить одежду
+        -- print('item:getVisual():toString() ', item:getVisual():toString())
+        print('item:getColor() ', item:getColor())
+        -- print('item:getColor():toString() ', item:getColor():toString())
+        if item:isCooked() then
+            resultTable['isCooked'] = item:isCooked()
+            resultTable['cookedString'] = item:getCookedString()
+        end
+        if item:isBurnt() then
+            resultTable['isBurnt'] = item:isBurnt()
+            resultTable['burntString'] = item:getBurntString()
+        end
+        if item:IsDrainable() then
+            local drainableItem = item --[[@as DrainableComboItem]]
+            resultTable['delta'] = drainableItem:getDelta()
+        end
+        if item:IsFood() then -- TODO: обрабатывать все свойства
+            local foodItem = item --[[@as Food]]
+            resultTable['hunger'] = foodItem:getHungChange()
+        end
+        table.insert(typeTable, resultTable)
         self.modData[type] = typeTable
     end
     self:setModData()
--- media/lua/client/ZipContainer_hooks.lua
+++ media/lua/client/ZipContainer_hooks.lua
@@ -20,6 +20,9 @@ local PatchPane = {}
 local PatchPage = {}
 local PathcTA = {}
 
+---@type InventoryItem[]
+local qItemsList = {}
+
 ---@param pane ISInventoryPane
 ---@param page ISInventoryPage
 local function refreshContainer(pane, page)
@@ -27,7 +30,7 @@ local function refreshContainer(pane, page)
     local container = pane.inventory
     local isCollapsed = page.isCollapsed
     if container:getType() == ZIP_CONTAINER_TYPE then
-        print('refreshContainer', container:getItems():size(), isCollapsed)
+        -- print('refreshContainer', container:getItems():size(), isCollapsed)
         if isCollapsed then
             container:removeAllItems()
         else
@@ -41,35 +44,46 @@ local function refreshContainer(pane, page)
 end
 
 ---@param character IsoPlayer
----@param item ComboItem
+---@param item InventoryItem
 ---@param sourceContainer ItemContainer
 ---@param targetContainer ItemContainer
 ---@param ta ISInventoryTransferAction
 local function onTransferComplete(character, item, sourceContainer, targetContainer, ta)
-    -- print('args: ', character, item, sourceContainer, targetContainer)
-    -- print('ta', bcUtils.dump(ta, 1))
-    print('ta.queueList', ta.queueList)
-    if ta.queueList then
-        print('ta.queueList2', #ta.queueList)
-    end
-    if sourceContainer:getType() == ZIP_CONTAINER_TYPE then
+    local isSourceZip = sourceContainer:getType() == ZIP_CONTAINER_TYPE
+    local isTargetZip = targetContainer:getType() == ZIP_CONTAINER_TYPE
+    if isSourceZip then
         local zipContainer = ZipContainer:new(sourceContainer)
         -- item:setContainer(ItemContainer itemContainer) -- NEED TRY
         zipContainer:removeItems({item})
         -- zipContainer:pickUpItems({item}, targetContainer)
         -- refreshContainer(self, self.inventoryPage)
-        -- TODO: добавить таймед экшин
-        return
+        -- return
     end
-    if targetContainer:getType() == ZIP_CONTAINER_TYPE then
+    if isTargetZip then
         local zipContainer = ZipContainer:new(targetContainer)
         zipContainer:addItems({item})
         -- zipContainer:putItems({item}, sourceContainer)
         -- print('zipContainer', bcUtils.dump(zipContainer.modData))
         -- refreshContainer(self, self.inventoryPage)
-        -- ISTimedActionQueue.add(ISInventoryTransferAction:new(playerObj, item, item:getContainer(), container)) -- TODO: Переписать
-        return
+        -- return
     end
+    -- local actionQueue = ISTimedActionQueue.getTimedActionQueue(character)
+    -- local indexSelf = actionQueue:indexOf(ta)
+    -- print('indexSelf', indexSelf)
+    -- print('actionQueue', #actionQueue.queue)
+    -- local isFinal = false
+    -- if isSourceZip or isTargetZip then
+    --     for idx, qItem in pairs(qItemsList) do
+    --         if qItem:getID() == item:getID() then
+    --             if idx == #qItemsList then
+    --                 isFinal = true
+    --             end
+    --         end
+    --     end
+    -- end
+    -- if isFinal then
+    --     qItemsList = {}
+    -- end
 end
 
 function PatchPage:clearMaxDrawHeight() -- SHOW
@@ -92,36 +106,39 @@ function PatchPane:refreshContainer()
     return ISInventoryPane_base.refreshContainer(self)
 end
 
--- ---@param items InventoryItem[]
--- ---@param targetContainer ItemContainer
--- function PatchPane:transferItemsByWeight(items, targetContainer)
---     ---@type ItemContainer
---     local sourceContainer = nil
---     if #items > 0 then
---         sourceContainer = items[1]:getContainer()
---     end
+---@param items InventoryItem[]
+---@param targetContainer ItemContainer
+function PatchPane:transferItemsByWeight(items, targetContainer)
+    -- ---@type ItemContainer
+    -- local sourceContainer = nil
+    -- if #items > 0 then
+    --     sourceContainer = items[1]:getContainer()
+    -- end
 
---     -- if sourceContainer and sourceContainer:getType() == ZIP_CONTAINER_TYPE then
---     --     local zipContainer = ZipContainer:new(sourceContainer)
---     --     -- item:setContainer(ItemContainer itemContainer) -- NEED TRY
---     --     zipContainer:pickUpItems(items, targetContainer)
---     --     refreshContainer(self, self.inventoryPage)
---     --     -- TODO: добавить таймед экшин
---     --     return
---     -- end
---     -- if sourceContainer and targetContainer:getType() == ZIP_CONTAINER_TYPE then
---     --     local zipContainer = ZipContainer:new(targetContainer)
---     --     zipContainer:putItems(items, sourceContainer)
---     --     refreshContainer(self, self.inventoryPage)
---     --     -- ISTimedActionQueue.add(ISInventoryTransferAction:new(playerObj, item, item:getContainer(), container)) -- TODO: Переписать
---     --     return
---     -- end
---     return ISInventoryPane_base.transferItemsByWeight(self, items, targetContainer)
--- end
+    -- if sourceContainer and sourceContainer:getType() == ZIP_CONTAINER_TYPE then
+    --     -- print('aaaaa')
+    --     qItemsList = items
+    --     -- local zipContainer = ZipContainer:new(sourceContainer)
+    --     -- item:setContainer(ItemContainer itemContainer) -- NEED TRY
+    --     -- zipContainer:pickUpItems(items, targetContainer)
+    --     -- refreshContainer(self, self.inventoryPage)
+    --     -- TODO: добавить таймед экшин
+    --     -- return
+    -- end
+    -- if targetContainer:getType() == ZIP_CONTAINER_TYPE then
+    --     -- print('bbbbbb')
+    --     qItemsList = items
+    --     -- local zipContainer = ZipContainer:new(targetContainer)
+    --     -- zipContainer:putItems(items, sourceContainer)
+    --     -- refreshContainer(self, self.inventoryPage)
+    --     -- ISTimedActionQueue.add(ISInventoryTransferAction:new(playerObj, item, item:getContainer(), container)) -- TODO: Переписать
+    --     -- return
+    -- end
+    return ISInventoryPane_base.transferItemsByWeight(self, items, targetContainer)
+end
 
 function PathcTA:new(character, item, srcContainer, destContainer, time)
     local o = ISInventoryTransferAction_base.new(self, character, item, srcContainer, destContainer, time)
-    print('o.qued', bcUtils.dump(o.queueList))
     o.onCompleteFunc = onTransferComplete
     o.onCompleteArgs = {character, item, srcContainer, destContainer, o}
     return o
@@ -131,7 +148,7 @@ end
 local makeHooks = function ()
     print('makeHooks')
     ISInventoryPane.refreshContainer = PatchPane.refreshContainer
-    -- ISInventoryPane.transferItemsByWeight = PatchPane.transferItemsByWeight
+    ISInventoryPane.transferItemsByWeight = PatchPane.transferItemsByWeight
 
     ISInventoryPage.selectContainer = PatchPage.selectContainer
     ISInventoryPage.setMaxDrawHeight = PatchPage.setMaxDrawHeight
@@ -142,7 +159,7 @@ end
 local removeHooks = function ()
     print('removeHooks')
     ISInventoryPane.refreshContainer = ISInventoryPane_base.refreshContainer
-    -- ISInventoryPane.transferItemsByWeight = ISInventoryPane_base.transferItemsByWeight
+    ISInventoryPane.transferItemsByWeight = ISInventoryPane_base.transferItemsByWeight
 
     ISInventoryPage.selectContainer = ISInventoryPage_base.selectContainer
     ISInventoryPage.setMaxDrawHeight = ISInventoryPage_base.setMaxDrawHeight
@@ -157,3 +174,9 @@ GremoveHooks = removeHooks
 -- Events.OnCreateUI.Add(makeHooks)
 Events.OnGameStart.Add(makeHooks)
 
+-- TODO: добавить логирование когда ТА завершилось. Для этого нужно сделать иньекцию в:
+-- ISInventoryTransferAction:stop() когда ТА завершилась преждевременно
+-- ISInventoryTransferAction:perform() когда ТА завершилась нормально
+-- Нужно посчитать сколько айтемов завершилось нормально и залогировать таблицу моддаты
+-- Очередь хранится в ISTimedActionQueue.getTimedActionQueue(character).queue
+-- Сложная задача. Оставим на потом
